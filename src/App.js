799e2334164d4a92f16cf4a29073dcba16a50717
MaxStrandberg <Max.Strandberg@myy.haaga-helia.fi>
2018-01-31 13:24:36 +0200

    img folder added

--- src/App.js
+++ src/App.js
@@ -6,11 +6,8 @@ class App extends Component {
     return (
       <div>
       <div class="container-fluid" >
-  <h1>Bootstrap Affix Example</h1>
-  <h3>Fixed (sticky) navbar on scroll</h3>
-  <p>Scroll this page to see how the navbar behaves with data-spy="affix".</p>
-  <p>The navbar is attached to the top of the page after you have scrolled a specified amount of pixels.</p>
-</div>
+      <img src="img/kuva.jpg" width="100%" height="150px" />
+      </div>
 
 <nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="197">
   <ul class="nav navbar-nav">
